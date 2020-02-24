77b9f973bd5dc5fb8881fe809f4d2f2b6c49d899
An Nguyen <an.nguyen6@mavs.uta.edu>
2020-02-24 02:35:13 -0600

    Add profile fragment

---
+++ app/src/main/java/com/example/bookitup/ui/profile/ProfileFragment.java
@@ -0,0 +1,30 @@
+package com.example.bookitup.ui.profile;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+import com.google.android.material.textfield.TextInputEditText;
+
+import com.example.bookitup.R;
+
+public class ProfileFragment extends Fragment {
+    private ProfileViewModel profileViewModel;
+    private TextView forgotPassword;
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_edit_profile, container, false);
+
+
+    }
+
+}
